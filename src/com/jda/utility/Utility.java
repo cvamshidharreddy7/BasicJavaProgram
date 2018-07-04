d33bee87ccd9022a021ed913a2eecd5737b922eb
Vamshidhar Reddy <cvamshidharreddy7@gmail.com>
2018-07-05 00:00:43 +0530

    Update Utility.java

--- src/com/jda/utility/Utility.java
+++ src/com/jda/utility/Utility.java
@@ -10,10 +10,30 @@ public class Utility {
 	public String inputName(){
 		return scanner.next();
 	}
-			public  String replaceString(String x,String name){
-					x = x.replaceAll("<<UserName>>",name);
+	public  String replaceString(String x,String name){
+	          x = x.replaceAll("<<UserName>>",name);
 					return x;
 					
 			}
+	
+		public int numberOfFlips() {
+			return scanner.nextInt();
+		}
+		public double randomNumberGenerator(){
+			return Math.random();
+		}
+	public int getYear(){
+					return scanner.nextInt();
+				}
+				
+				public boolean IsLeapYear( int year) {
+					boolean flag = false;
+					if(year%400==0) flag = true;
+					else if(year%100 ==0) flag = false;
+					else if(year%4==0) flag = true;
+					else flag = false;
+					return flag;
+				}
+	
 		
 }
