3323084388965868a2719460d88bb1be534c7846
cvamshidharreddy7 <cvamshidharreddy7@gmail.com>
2018-07-05 14:38:57 +0530

    Gambler problem

--- src/com/jda/functionalprograms/PrimeFactorisation.java
+++ src/com/jda/functionalprograms/PrimeFactorisation.java
@@ -3,11 +3,9 @@ package com.jda.functionalprograms;
 import com.jda.utility.Utility;
 
 public class PrimeFactorisation {
-			public static void main(String[] args){
-				Utility utility = new Utility();
-					int Number = utility.inputNumber();
-					utility.primeFactorisation(Number);
-					
-					
-			}
+	public static void main(String[] args) {
+		Utility utility = new Utility();
+		int Number = utility.inputNumber();
+		utility.primeFactorisation(Number);
+	}
 }
--- src/com/jda/utility/Utility.java
+++ src/com/jda/utility/Utility.java
@@ -57,40 +57,81 @@ public class Utility {
 		power = power * 2;
 		return power;
 	}
-	public double getInverse(int number){
-			Double number1 = Double.valueOf(number);
-			
-			return 1/number1 ;
+
+	public double getInverse(int number) {
+		Double number1 = Double.valueOf(number);
+
+		return 1 / number1;
 	}
-	double harmonicNumber = 0; 
-	public double harmonicValue(int number){
-			for(int i=1;i<=number;i++){
-				harmonicNumber += getInverse(i);
-			}
-			return harmonicNumber;
+
+	double harmonicNumber = 0;
+
+	public double harmonicValue(int number) {
+		for (int i = 1; i <= number; i++) {
+			harmonicNumber += getInverse(i);
+		}
+		return harmonicNumber;
 	}
+
 	int numberOfTwos = 0;
-	public int makeOddNumber(int number){
-			while(number%2==0){
-				number = number/2;
-				numberOfTwos++;
-			}
-			for(int i=0;i<numberOfTwos;i++){
-				System.out.print("2" + " ");
-			}
-			return number;
+
+	public int makeOddNumber(int number) {
+		while (number % 2 == 0) {
+			number = number / 2;
+			numberOfTwos++;
+		}
+		for (int i = 0; i < numberOfTwos; i++) {
+			System.out.print("2" + " ");
+		}
+		return number;
 	}
-	public int primeFactorisation(int number){
-			number = makeOddNumber(number);
-			for(int i=3;i<=Math.sqrt(number); i+=2){
-				while(number%i == 0){
-					System.out.print(i+ " ");
+
+	public int primeFactorisation(int number) {
+		number = makeOddNumber(number);
+		for (int i = 3; i <= Math.sqrt(number); i += 2) {
+			while (number % i == 0) {
+				System.out.print(i + " ");
 				number /= i;
-				}
-				
-				
 			}
-			return 0;
+
+		}
+		return 0;
+	}
+
+	// Gambler problem
+	public int getStake() {
+		return scanner.nextInt();
+	}
+
+	public int getGoal() {
+		return scanner.nextInt();
+	}
+
+	public int getNumber() {
+		return scanner.nextInt();
+	}
+
+	public void gamblingAlgo(int stake, int goal, int number) {
+
+		int wins = 0;
+		int losses = 0;
+
+		
+		int total = wins+losses;
+		while(stake !=0) {
+			int randNum = (int) (Math.random() * 2);
+			if (randNum == 0) {
+				// win condition
+				stake++;
+				wins++;
+			} else {
+				// lose condition
+				stake--;
+				losses++;
+			}
+		}
+		System.out.println(wins + "wins and " + losses +"losses");
+		
 	}
 
 }
