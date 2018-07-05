5e77b7eb546461508aa6ca46c13712858107e1df
cvamshidharreddy7 <cvamshidharreddy7@gmail.com>
2018-07-05 10:51:55 +0530

    Another commit

---
+++ src/com/jda/functionalprograms/PowersOfTwo.java
@@ -0,0 +1,17 @@
+package com.jda.functionalprograms;
+
+import com.jda.utility.Utility;
+
+public class PowersOfTwo {
+			
+
+			public static void main(String[] args){
+				Utility utility = new Utility();
+				
+				  	int Number = utility.inputNumber();
+					
+					for(int i=0;i<Number;i++){
+					System.out.println(utility.getPower(Number));
+					}
+			}
+}
--- src/com/jda/utility/Utility.java
+++ src/com/jda/utility/Utility.java
@@ -2,6 +2,8 @@ package com.jda.utility;
 
 import java.util.Scanner;
 
+import com.jda.functionalprograms.PowersOfTwo;
+
 public class Utility {
 	Scanner scanner;
 	public Utility(){
@@ -14,6 +16,33 @@ public class Utility {
 					x = x.replaceAll("<<UserName>>",name);
 					return x;
 					
+			}
+			public int numberOfFlips() {
+				return scanner.nextInt();
+			}
+			public double randomNumberGenerator(){
+				return Math.random();
+			}
+			public int getYear(){
+				return scanner.nextInt();
+			}
+			
+			public boolean IsLeapYear( int year) {
+				boolean flag = false;
+				if(year%400==0) flag = true;
+				else if(year%100 ==0) flag = false;
+				else if(year%4==0) flag = true;
+				else flag = false;
+				return flag;
+			}
+			public int inputNumber(){
+				return scanner.nextInt();
+			}
+			PowersOfTwo powersOfTwo = new PowersOfTwo();
+			int power = 1;
+			public int getPower(int Number){
+				power = power*2;
+				return power;
 			}
 		
 }
