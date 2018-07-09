c5e40cf7619ec8589bd45d7e4880949e2f51b3f1
cvamshidharreddy7 <cvamshidharreddy7@gmail.com>
2018-07-09 14:48:05 +0530

    Sum equal to zero

---
+++ src/com/jda/functionalprograms/DIstanceProblem.java
@@ -0,0 +1,11 @@
+package com.jda.functionalprograms;
+
+import com.jda.utility.Utility;
+
+public class DIstanceProblem {
+		public static void main(String[] args){
+			Utility utility = new Utility();
+			utility.getCoordinates();
+			
+		}
+}
---
+++ src/com/jda/functionalprograms/StopWatch.java
@@ -0,0 +1,10 @@
+package com.jda.functionalprograms;
+
+import com.jda.utility.Utility;
+
+public class StopWatch {
+			public static void main(String[] args){
+				Utility utility = new Utility();
+				System.out.println("Elapsed time is " +utility.stopWatch());
+			}
+}
