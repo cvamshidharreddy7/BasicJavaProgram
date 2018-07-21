b0357933530863374e06b8e7dc8fa9540cff1a1b
Vamshidhar Reddy Cherukupalli <Vamshidhar.Cherukupalli@jda.com>
2018-07-21 22:37:35 +0530

    Calendar again

--- src/com/jda/DataStructure_programs/Calendar.java
+++ src/com/jda/DataStructure_programs/Calendar.java
@@ -25,26 +25,62 @@ public class Calendar {
 				System.out.print("   ");
 				
 			}
-			String[][] calendar = new String[7][7];
+		
 			int k=firstDay;
+			String[] temp=new String[42];
+			String[][] calendarArray = new String[6][7];
 			
 				for(int i=0;i<firstDay;i++){
-					calendar[0][i] = "  ";
+					
+					temp[i] = "  ";
 				}
-				
+			
+			
 			
 			for(int i=1;i<=days[month];i++) {
 				if(i<10) {
-					System.out.print("  "+i);					
+					System.out.print("  "+i);	
+				temp[i+firstDay-1] = Integer.toString(i);
 					if(((i+firstDay)%7 == 0)) { 						
 						System.out.println();					
 					}
 				}
 				else {
 					System.out.print(" "+i);
+					temp[i+firstDay-1] = Integer.toString(i);
 				if(((i+firstDay)%7 == 0)) System.out.println();
 				}
 			}
+			System.out.println();
+			for(int i=0;i<temp.length;i++) {
+				if(temp[i]==null) temp[i] = " ";
+			}
+		/*for(int i=0;i<temp.length;i++) {
+				System.out.print(temp[i]+",");
+			}
+			*/
+			//Setting values in temp which are null to zero
+			for(int i=0;i<temp.length;i++) {
+				if(temp[i]==null) {
+					temp[i] ="0";
+				}
+			}
+		System.out.println();
+			int count =0;
+			for(int i=0;i<6;i++) {
+				for(int j=0;j<7;j++) {
+					if(count == temp.length) break;
+					calendarArray[i][j] = temp[count];
+					count++;
+				}
+			}
+			for(int i=0;i<6;i++) {
+				for(int j=0;j<7;j++) {
+					System.out.print(calendarArray[i][j] + "   ");
+				
+					}System.out.println();
+			}
+			
 	    
 	}
 }
